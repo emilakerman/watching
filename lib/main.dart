b673289e898e6fd4ae2e7b2c09abab61971463f2
emilakerman <emilaakerman@gmail.com>
2024-03-09 09:19:43 +0100

    separated app from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:watching/app.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(const App());
