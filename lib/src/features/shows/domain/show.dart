12732063eef0042b462fc8c7b75a1f66ce8620aa
emilakerman <emilaakerman@gmail.com>
2024-03-23 14:02:14 +0100

    added freezed model with json serialiszable for local show model

---
+++ lib/src/features/shows/domain/show.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'show.freezed.dart';
+part 'show.g.dart';
+
+@freezed
+class Show with _$Show {
+  const factory Show({
+    required int id,
+    required String name,
+    required String type,
+    required String language,
+    required List<String> genres,
+    required String summary,
+    // Add other fields as needed
+  }) = _Show;
+
+  factory Show.fromJson(Map<String, dynamic> json) => _$ShowFromJson(json);
+}
