0386a3dfc7fa83942d76e07a7c27fe5f0559cf6f
emilakerman <emilaakerman@gmail.com>
2024-08-20 16:19:38 +0200

    shortened method

--- lib/src/features/shows/data/node_express_repository.dart
+++ lib/src/features/shows/data/node_express_repository.dart
@@ -2,45 +2,29 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
-import 'package:logger/logger.dart';
 
 class NodeExpressRepository {
   NodeExpressRepository() {
-    _client = dotenv.env['nodeEndpoint']!;
-    _token = dotenv.env['secretToken']!;
+    this._client = dotenv.env['nodeEndpoint']!;
+    this._token = dotenv.env['secretToken']!;
   }
 
   /// -- Fetches Public Users For the Leaderboard Feature --
   Future<List<dynamic>?> fetchAllPublicUsers() async {
     final String url = '$_client$_token/public-users';
     try {
-      final Response response = await http.get(Uri.parse(url));
+      final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
-        final dynamic decodedData = json.decode(response.body);
-
-        // Ensure the decoded data is a list of maps
-        if (decodedData is List) {
-          // Extract the 'id' from each user, assuming 'id' is a string
-          final List<dynamic> ids = decodedData
-              .where((user) =>
-                  user is Map<String, dynamic> && user.containsKey('id'))
-              .map<dynamic>((user) => user['id'])
-              .toList();
-
-          Logger().d('Fetched public users successfully!: $ids');
-          return Future.value(ids); // Wrap ids in a Future
-        } else {
-          Logger().d('Unexpected data format: ${decodedData.runtimeType}');
-          return Future.value(null); // Return null as a Future
-        }
-      } else {
-        Logger().d('Failed to fetch public users');
-        return Future.value(null); // Return null as a Future
+        final decodedData = json.decode(response.body);
+        return decodedData is List
+            ? decodedData
+                .whereType<Map<String, dynamic>>()
+                .map((user) => user['id'])
+                .toList()
+            : null;
       }
-    } catch (error) {
-      Logger().d('Error fetching public users: $error');
-      return Future.value(null); // Return null as a Future
-    }
+    } catch (error) {}
+    return null;
   }
 
   late final String _client;
